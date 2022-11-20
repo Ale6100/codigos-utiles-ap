61d9cbf2fbfa6b554d7d623e9355e93c15306ad8
Alejandro <alejandro_portaluppi@outlook.com>
2022-11-20 19:14:59 -0300

    Agregado de funciones y tratamiento de errores

--- index.js
+++ index.js
@@ -1,12 +1,18 @@
 "use strict";
 
-// Recibe un número entre 0 y 100. Devuelve true el n% de las veces
+
+//* ----- NÚMEROS -----
+
+
+// Recibe un número entre 0 y 100 (como number o string). Devuelve true el n% de las veces
 const probabilidadDeN = (n) => {
+    if (n < 0 || n > 100) throw new Error('probabilidadDeN debe recibir un número entre 0 y 100')
     return Math.random()*100 <= n
 }
 
 // Recibe dos números enteros y de vuelve un número entero al azar entre ellos
 const numeroEnteroAlAzar = (a, b) => {
+    if (a !== parseInt(a) || b !== parseInt(b)) throw new Error('numeroEnteroAlAzar debe recibir dos números enteros')
     const random = Math.random() // Número al azar entre 0 y 1 (sin incluir el 1)
     const randomAmpliado = random*(Math.abs(b-a)+1) // Entre 0 y |b-a|+1 (sin incluir el |b-a|+1). Aumento el rango
     const randomEntero = parseInt(randomAmpliado) // Número entero entre 0 y |b-a|
@@ -16,27 +22,22 @@ const numeroEnteroAlAzar = (a, b) => {
 
 // Recibe dos números enteros y devuelve un número al azar entre ellos
 const numeroAlAzar = (a, b) => {
+    if (a !== parseInt(a) || b !== parseInt(b)) throw new Error('numeroAlAzar debe recibir dos números enteros')
     const random = Math.random()
     const randomAmpliado = random*(Math.abs(b-a)) //  Entre 0 y |b-a|. En este caso no es necesario sumar 1
     const numeroMasChico = a < b ? a : b
     return randomAmpliado + numeroMasChico
 }
 
-// Recibe un array y devuelve un elemento al azar
-const elementoAlAzar = (array) => {
-    const random = Math.random() // Número al azar entre 0 y 1 (sin incluir el 1)
-    const randomAmpliado = random*array.length // Número al azar entre 0 y array.length (sin incluir el array.length)
-    const indexAlAzar = parseInt(randomAmpliado) // Número entero al azar entre 0 y array.length-1. Observar que es una posición i-ésima al azar del array
-    return array[indexAlAzar] // Gracias al indice al azar se devuelve un elemento al azar
-}
-
-// Si N es divisor de n, devuelve true
+// Recibe dos números (como number o string). Si N es divisor de n, devuelve true
 const esDivisor = (n, N) => { 
-    return n%N == 0 ? true : false
+    return n%N == 0
 }
 
-// Recibe un número entero y devuelve un array con todos sus divisores
+// Recibe un número entero (como number o string) y devuelve un array con todos sus divisores
 const divisores = (n) => {
+    if (n != parseInt(n)) throw new Error('divisores debe recibir un número entero')
+    if (n == 0) return Infinity
     const signoDen = n/Math.abs(n)
     n = Math.abs(n)
     let divisoresDeN = []
@@ -49,8 +50,9 @@ const divisores = (n) => {
     return divisoresDeN // Por ejemplo si n=12, divisoresDeN = [1, 2, 3, 4, 6, 12]
 }
 
-// Recibe un número natural y devuelve un array con dos valores [a, b] tal que n=a*b, donde a y b son los valores más cercanos posibles
+// Recibe un número natural (como number o string) y devuelve un array con dos valores [a, b] tal que n=a*b, donde a y b son los valores más cercanos posibles
 const factoresMasCercanos = (n) => {
+    if (n != parseInt(n) || n <= 0) throw new Error('factoresMasCercanos debe recibir un número natural')
     let divisoresDeN = divisores(n)
     let res = divisoresDeN 
         
@@ -80,14 +82,46 @@ const factoresMasCercanos = (n) => {
     return res // Si n=12, entonces retorna [4, 3]
 }
 
-// Redondea a dos decimales. Fuente: https://www.delftstack.com/es/howto/javascript/javascript-round-to-2-decimal-places/#uso-de-la-funci%C3%B3n-personalizada-para-redondear-un-n%C3%BAmero-a-2-decimales-en-javascript
+// Recibe un número (como number o string) y lo devuelve redondeado a dos decimales. Fuente: https://www.delftstack.com/es/howto/javascript/javascript-round-to-2-decimal-places/#uso-de-la-funci%C3%B3n-personalizada-para-redondear-un-n%C3%BAmero-a-2-decimales-en-javascript
 const redondear = (num) => {
+    if (num != parseFloat(num)) throw new Error('redondear debe recibir un número')
     return +(Math.round(num + "e+2")  + "e-2")
 };
 
-// Recibe tres números. Devuelve un array de números desde el origen hasta el final (sin incluir) solicitado, considerando un espaciado entre valores consecutivos
+// Recibe un número natural o cero (como number o string). Devuelve el factorial de dicho número.
+const factorial = (num) => {
+    if (num != parseInt(num) || num < 0) throw new Error('factorial debe recibir un número natural o cero')
+    let r = 1
+    for (let i = 1; i<=num; i++) {
+        r = i*r
+    }
+    return r
+}
+
+// Recibe un número (como number o string). Devuelve true si es par, pero falso si es impar
+const esPar = (num) => {
+    if (num != parseFloat(num)) throw new Error('esPar debe recibir un número')
+    return num%2 == 0
+}
+
+
+//* ----- ARRAYS -----
+
+
+// Recibe un array y devuelve un elemento al azar
+const elementoAlAzar = (array) => {
+    if (typeof array == "number" || typeof array == "string") throw new Error('elementoAlAzar debe recibir un array')
+    const random = Math.random() // Número al azar entre 0 y 1 (sin incluir el 1)
+    const randomAmpliado = random*array.length // Número al azar entre 0 y array.length (sin incluir el array.length)
+    const indexAlAzar = parseInt(randomAmpliado) // Número entero al azar entre 0 y array.length-1. Observar que es una posición i-ésima al azar del array
+    return array[indexAlAzar] // Gracias al indice al azar se devuelve un elemento al azar
+}
+
+// Recibe tres números. El tercero debe ser un número natural. Devuelve un array de números desde el origen hasta el final (sin incluir) solicitado, considerando un espaciado entre valores consecutivos
 // Si no se pide el espaciado se sobreentiende que es de 1
 const arange = (origen, final, espaciado = 1) => {
+    if (origen !== parseFloat(origen) || final !== parseFloat(final)) throw new Error('arange debe recibir números')
+    if (espaciado !== parseFloat(espaciado) || espaciado <= 0) throw new Error('El tercer parámetro de arange debe ser un número mayor a cero')
     const array = []
     for (let i=origen; i<final; i+=espaciado) {
         array.push(i)
@@ -95,8 +129,11 @@ const arange = (origen, final, espaciado = 1) => {
     return array
 }
 
-// Recibe tres números. Devuelve un array de números equiespaciados desde el origen hasta el final solicitado, considrando la densidad (cantidad) de valores pedidos
+// Recibe tres números. Los primeros dos deben ser distintos. El tercero debe ser positivo. Devuelve un array de números equiespaciados desde el origen hasta el final solicitado, considrando la densidad (cantidad) de valores pedidos
 const linspace = (origen, final, densidad) => {
+    if (origen !== parseFloat(origen) || final !== parseFloat(final) || densidad !== parseFloat(densidad)) throw new Error('linspace debe recibir números')
+    if (origen == final) throw new Error('Los primeros dos parámetros de linspace deben ser números distintos')
+    if (densidad < 0) throw new Error('El tercer parámetro de linspace debe ser un número positivo')
     const espaciado = (final-origen)/(densidad-1)
     const array = []
     for (let i=origen; i<=final; i+=espaciado) {
@@ -107,6 +144,7 @@ const linspace = (origen, final, densidad) => {
 
 // Recibe un array y lo devuelve mezclado
 const mezclar = (array) => {
+    if (typeof array == "number" || typeof array == "string") throw new Error('mezclar debe recibir un array')
     const arrayMezclado = []
     array.forEach(() => {
         while (true) {
@@ -120,16 +158,43 @@ const mezclar = (array) => {
     return arrayMezclado
 }
 
-// Recibe un número entero mayor o igual a cero. Devuelve el factorial de dicho número.
-const factorial = (num) => {
-    let r = 1
-    for (let i = 1; i<=num; i++) {
-        r = i*r
+// Recibe un array y un número natural n (como number o string). Devuelve n elementos al azar del array
+const obtenerNElementos = (array, n) => {
+    if (typeof array == "number" || typeof array == "string") throw new Error('El primer parámetro de obtenerNElementos debe ser un array')
+    if (n != parseInt(n) || n <= 0 || array.length < n) throw new Error('El segundo parámetro de obtenerNElementos debe ser un número natural mayor a la longitud del array en el primer parámetro')
+    const elementos = []
+    while (elementos.length < n) {
+        const elementoRandom = elementoAlAzar(array)
+        if (!elementos.includes(elementoRandom)) {
+            elementos.push(elementoRandom)
+        }
     }
-    return r
+    return elementos
 }
 
-// Recibe un número. Devuelve true si es par, pero falso si es impar
-const esPar = (num) => {
-    return num%2 == 0
+
+//* ----- STRINGS -----
+
+
+// Recibe un número n natural (como number o string), devuelve un string aleatorio de longitud n
+const stringAleatorio = (n) => {
+    if (n != parseInt(n) || n <= 0) throw new Error('stringAleatorio debe recibir número natural')
+    const simbolos = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789¡!¿?@#$%&()+-=*,.;:_"
+    let stringRandom = ""
+    for (let i=1; i<=n; i++) {
+        stringRandom += simbolos[parseInt(simbolos.length*Math.random())]
+    }
+    return stringRandom
+}
+
+
+//* ----- OTROS -----
+
+
+// Devuelve un color rgb al azar
+const colorRandom = () => {
+    const red = parseInt(Math.random()*256)
+    const green = parseInt(Math.random()*256)
+    const blue = parseInt(Math.random()*256)
+    return `rgb(${red}, ${green}, ${blue})`
 }
